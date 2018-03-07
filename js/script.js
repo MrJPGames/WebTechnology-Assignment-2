b27f9bb9a31d64f489e7e050b2e72e8ee6e25090
Jasper W. D. M. Peters <jasper@fizazy.com>
2018-03-07 15:49:29 +0100

    NOT FINISHED FAR FROM IT OKE!!!!!!!!!Y

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
-//Wait for DOM to load
-$(document).ready(function() {
-    var style = document.createElement("style");
-    document.head.appendChild(style);
-    
-    //When context menu is loaded
-    document.oncontextmenu = function (e) {
-        console.log(e);
-        
-        style.sheet.insertRule(e.srcElement.tagName+'{color:red}');
+//This object creates the Context menu shown to user when right clinking page
+class ContextMenu {
+    constructor(tagName, idName,mousePosition){
+        this.title = tagName;
+        this.posX = mousePosition.x;
+        this.posY = mousePosition.y;
     }
-});
+
+    show(){
+        this.domContextMenu.style.display = "block";
+        this.domContextMenu.style.position = "absolute";
+        this.domContextMenu.style.top = this.posX;
+        this.domContextMenu.style.bottom = this.posY;
+    }
+}
+
+
+var style = document.createElement("style");
+document.head.appendChild(style);
+
+//When right click is pressed
+document.oncontextmenu = function (e) {
+    console.log(e);
+    
+
+    contextMenu = ContextMenu(e.srcElement.tagName, e.srcElement.idName, {"x": screenX, "y": screenY} );
+
+    contextMenu.show();
+    style.sheet.insertRule(e.srcElement.tagName+'{color:red}');
+    return false; //Stop normal context menu from opening
+}
