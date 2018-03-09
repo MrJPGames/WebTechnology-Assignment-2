21955b79358295c50a6c60ba0de737c8018b542f
maarten6 <32553324+maarten6@users.noreply.github.com>
2018-03-09 19:18:23 +0100

    made general class

--- js/dynamic.js
+++ js/dynamic.js
@@ -2,20 +2,20 @@
  * This script will place content to summary.html dynamically.
  */ 
 
-//Class of security attacks
-class Attack {
-    constructor(name) {
+//General class
+class General {
+    //Name of the class
+    constructor (name) {
         this.name = name;
     }
-    // create HTML section of the content
-    content(title, description) {
-        var section = document.createElement("section");
-        section.setAttribute("id", this.name);
-        var sectionTitle = document.createElement("h3");
-        sectionTitle.appendChild(document.createTextNode(this.title));
-        section.appendChild(sectionTitle);
-        var paragraph = document.createElement("p");
-        paragraph.appendChild(document.createTextNode(this.description));
-        section.appendChild(paragraph);
+    //Create h3 section title 
+    createTitle (head) {
+        this.title = document.createElement("H3");
+        this.title.appendChild(document.createTextNode(this.head));
+    }
+    //Content in a paragraph
+    createParagraph (content) {
+        this.paragraph = document.createElement("P");
+        this.paragraph.appendChild(document.createTextNode(this.content));
     }
 }
