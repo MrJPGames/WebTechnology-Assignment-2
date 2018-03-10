3f4c7ba8dbd076bff4437203ba68dadd198ca739
maarten6 <32553324+maarten6@users.noreply.github.com>
2018-03-10 22:14:56 +0100

    dynamic.js done, no CSS yet tho

--- js/dynamic.js
+++ js/dynamic.js
@@ -5,16 +5,18 @@
 //General class for general information (paragraph and title)
 class General {
     //Name of the class
-    constructor (name) {
+    constructor (name, head, content) {
         this.name = name;
+        this.createTitle(head);
+        this.createSection(content);
     }
     //Create h3 section title 
     createTitle (head) {
         this.title = document.createElement("H3");
         this.title.appendChild(document.createTextNode(head));
     }
-    //Content in a paragraph (with section)
-    createParagraph (content) {
+    //Content in a paragraph (within section)
+    createSection (content) {
         this.section = document.createElement("SECTION");
         this.paragraph = document.createElement("P");
         this.paragraph.appendChild(document.createTextNode(content));
@@ -22,39 +24,27 @@ class General {
     }
 }
 
-try {
-history = new General("History");
-console.log(history.name);
-history.createTitle("First form of hacking");
-}
-catch (E) {
-    console.log(E);
-}
-
-/*
 //Class have specific field: as a list (key-value pairs)
 class ListText extends General {
     constructor (name, head, content, itemsList) {
         super(name, head, content);
         //Create an object, if listItems was an array/object
         if (typeof itemsList === "object") {
-            this.list = new Object();
-            for (var key in this.itemsList) 
-                this.list.key = this.itemsList.key;  
+            this.list = itemsList;
+
             this.createList();
         }
     }
     //Create html list with list object
     createList() {
         this.ul = document.createElement("UL");
-        this.li = document.createElement("LI");
         
         for (var key in this.list) {
-            this.listText = document.createTextNode(key + ' - ' + this.list.key);
+            this.li = document.createElement("LI");
+            this.listText = document.createTextNode(key + ' - ' + this.list[key]);
             this.li.appendChild(this.listText);
-            //Append the list item to the ul, then clear li-tag
+            //Append the list item to the ul
             this.ul.appendChild(this.li);
-            this.li.removeChild(this.listText);
         }
     }
 }
@@ -65,18 +55,15 @@ class ImageText extends General {
         super (name, head, content);
         //Only accept specific imagePath
         if (typeof imagePath === "string" && imagePath.endsWith(".jpg") && typeof alternative === "string") 
-            this.createImage(imagePath); 
+            this.createImage(imagePath, alternative); 
     }
-    createImage(imagePath) {
+    createImage(imagePath, alternative) {
         this.img = document.createElement("IMG");
         this.img.setAttribute("src", imagePath);
         this.img.setAttribute("alt", alternative);
     }
 }
 
-*/
-
-/*
 //Create the necessary objects 
 window.securityList = new Object({
     DoS: `Denial of service attacks (DoS) are designed to make a machine or network resource unavailable 
@@ -97,7 +84,7 @@ window.importanceList = new Object({
     should always be able to access that system.` 
 });
 
-history = new General("History", "First form of hacking", `The first security threats were actually created before 
+pageHistory = new General("History", "First form of hacking", `The first security threats were actually created before 
 personal computers were a common household item. Decades ago, criminals often looked to tap into phone systems. 
 Starting in the 1960s, AT&T decided to closely monitor calls in order to catch “phone freaks.” These “phreakers”, 
 as they were called, used “blue boxes” to generate the right tone to get free calls. This surveillance eventually 
@@ -105,7 +92,7 @@ led to 200 convictions. Not long after, a man named John Draper found a way to d
 and a toy whistle found in Cap’n Crunch cereal. The tone was used to unlock the AT&T network. 
 As serious as these threats were, the focus on phone networks would soon pave the way for greater risks to computers.`);
 
-encryption = new General("Encryption", "Encryption", `Encryption ensures that one party can safely communicate a message or piece of 
+pageEncryption = new General("Encryption", "Encryption", `Encryption ensures that one party can safely communicate a message or piece of 
 data from one system to another. We often see this explained as stoping people 
 from interfere in this communication. However this is not entirely accurate.
 As encryption itself does not stop any third party from seeing the encrypted data.
@@ -114,33 +101,47 @@ And it also protects against any third party sending data pretending to be someo
 In this way though a third party can still interfere with the communications, because these 
 are unintelligable for them it is has the same result as if they were unable to interfere.`);
 
-importance = new ListText("Importance", "Importance of security", `Imporance has become increasingly important as more and 
+pageImportance = new ListText("Importance", "Importance of security", `Imporance has become increasingly important as more and 
 more computer systems are being utilzed. With more and more devices going on the internet, know as the Internet of Things, 
 the attack surface for hackers has also increased. Even cars have become a target for hackers as the computers inside 
-them are getting increasingly complex, but also increasingly connected with the internet.`,window.securityList);
+them are getting increasingly complex, but also increasingly connected with the internet.`,window.importanceList);
 
-security = new ListText("Security", "Security Attacks", `Computer security, or more formally known as IT security, is defined as the protection 
+pageSecurity = new ListText("Security", "Security attacks", `Computer security, or more formally known as IT security, is defined as the protection 
 of computer systems from the theft and damage to their hardware, software or information, 
 as well as from disruption or misdirection of the services they provide
 Thus, protection includes the physical and software side. Security is a growing field of interest and
 importance because we are relying more and more on these devices. These devices are connected to the 
-Internet, and that brings a lot of trouble with it. 
-In the next section we will discuss some common modern attacks.`, window.securityList);
+Internet, and that brings a lot of trouble with it.`, window.securityList); 
 
-economics = new ImageText("Economics", "Statistics about security", `The image hereunder points out the statistics of 
+pageEconomics = new ImageText("Economics", "Statistics about security", `The image hereunder points out the statistics of 
 the spending on cyber security in the recent years (in the U.S.) as a percentage of the GDP as 
-well as absolute in USD (dollars). We see an increasing spending over the years.`, "http://publications.atlanticcouncil.org/cyberrisks//images/figure-8.jpg");
-*/
+well as absolute in USD (dollars). We see an increasing spending over the years.`, 
+"http://publications.atlanticcouncil.org/cyberrisks//images/figure-8.jpg", "Cybersecurity spendings");
 
-/*Method to add siblings to the parent node
+
+//Method to add siblings to the parent node
 function addSibling (parent, obj) {
-    if (obj instanceof General) {
-        parent.appendChild(obj.title);
-        parent.appendChild(obj.section);
-    }
-    console.log(obj.name);
+    parent.appendChild(obj.title);
+    parent.appendChild(obj.section);
+    if(obj instanceof ListText) 
+        obj.section.appendChild(obj.ul);
+    else if(obj instanceof ImageText) 
+        obj.section.appendChild(obj.img);
 }
 
-//Put every object on the page: summary.html
-addSibling($("main")[0], history);
-*/
+/*THIS FUNCTION DOESNT WORK YET.............................
+//Put every object on the page: summary.html, first create an article
+//Loop through all global variables, adapted from: https://stackoverflow.com/questions/2226007/fetching-all-javascript-global-variables-in-a-page
+function retrieveGlobalVars(prefix) {
+    var globalVariables = [], global = window;
+    for (var value in global) {
+      if (value.indexOf(prefix) == 0) // check the prefix
+        globalVariables.push(global.variable);
+    }
+    return globalVariables;
+} */
+addSibling($("main")[0], pageSecurity);
+addSibling($("main")[0], pageEncryption);
+addSibling($("main")[0], pageHistory);
+addSibling($("main")[0], pageImportance); 
+addSibling($("main")[0], pageEconomics); 
