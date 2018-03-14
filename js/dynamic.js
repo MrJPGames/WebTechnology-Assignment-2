2fa419b2b359e5e190cafa0d55466acf58f11616
maarten6 <32553324+maarten6@users.noreply.github.com>
2018-03-14 19:07:34 +0100

    goku went super saiyan

--- js/dynamic.js
+++ js/dynamic.js
@@ -18,7 +18,7 @@ class General {
     //Content in a paragraph (within section), also the section has ID equal to the name
     createSection (content) {
         this.section = document.createElement("section");
-        this.section.id = this.name;
+        this.section.id = this.name.toLowerCase();
         this.paragraph = document.createElement("p");
         this.paragraph.appendChild(document.createTextNode(content));
         this.section.appendChild(this.paragraph);
@@ -33,6 +33,19 @@ class ListText extends General {
         if (typeof itemsList === "object") {
             this.list = itemsList;
             this.createList();
+
+            //Attaching the event listener for title of section (bubbling phase)
+            //Makes the list visible, depending on click
+            this.title.addEventListener("click", e => {
+                var visible = this.unorderedList.ul.style.display;
+                if(visible === "none") {
+                    this.unorderedList.ul.removeAttribute("style");
+                    this.title.innerText = this.title.innerText.replace("▼", "▲");
+                } else {
+                    this.unorderedList.ul.style.display = "none";
+                    this.title.innerText = this.title.innerText.replace("▲", "▼");
+                }
+            }, false);
         }
     }
     //Create 'html' list with list object
@@ -50,6 +63,7 @@ class List {
             this.li.appendChild(this.listText);
             //Append the list item to the ul
             this.ul.appendChild(this.li);
+            this.ul.style.display =  "none";
         }
     }
 }
@@ -57,44 +71,48 @@ class List {
 //E.g. economis page has statistic; an image (could apply to other pages as well)
 //Attach event listener to the title, so that image will be displayed afterwards
 class ImageText extends General {
-    constructor(name , head, content, imagePath, alternative) {
+    constructor(name , head, content, imagePath, alternative, thisId) {
         super(name, head, content);
         //Only accept specific imagePath, accept only .jpg extension
         if(typeof imagePath === "string" && imagePath.endsWith(".jpg") && typeof alternative === "string") {
-            this.createImage(imagePath, alternative); 
+            this.createImage(imagePath, alternative, thisId); 
 
             //Attaching the event listener for title of section (bubbling phase)
             //Makes the immage visible, depending on click
             this.title.addEventListener("click", e => {
                 var visible = this.image.img.style.display;
-                if(visible === "none")
+                if(visible === "none") {
                     this.image.img.removeAttribute("style");
-                else
+                    this.title.innerText = this.title.innerText.replace("▼", "▲");
+                } else {
                     this.image.img.style.display = "none";
+                    this.title.innerText = this.title.innerText.replace("▲", "▼");
+                }
             }, false);
         }
     }
-    createImage(imagePath, alternative) {
+    createImage(imagePath, alternative, thisId) {
         //Create image with Image class
-        this.image = new Image(imagePath, alternative);
+        this.image = new Image(imagePath, alternative, thisId);
     }
 }
 
 //Define the image clas
 class Image {
-    constructor(imagePath, alternative) {
+    constructor(imagePath, alternative, thisId) {
         //Image should be created, but not visible yet, only though interaction
         this.img = document.createElement("img");
         this.img.setAttribute("src", imagePath);
         this.img.setAttribute("alt", alternative);
-        this.img.style.display =  "none";
+        this.img.id = thisId;
+        this.img.style.display = "none";
     }
 }
 
 //Menu class to browse through sections
 class Menu {
     constructor() {
-        this.article = new Article();
+        this.article = new Article("Summary", "articleTitle");
         this.ul = document.createElement("ul");
         for(var i = 0; i < arguments.length; i++) 
             if(typeof arguments[i] === "string") 
@@ -103,9 +121,9 @@ class Menu {
     createMenu(item) {
         var li = document.createElement("li");
         var a = document.createElement("a");
-        a.setAttribute("href", "#" + item);
+        a.setAttribute("href", "#" + item.toLowerCase());
         a.appendChild(document.createTextNode(item));
-        li.appendChild(a);
+        li.appendChild(a);  
         this.ul.appendChild(li);
 
         //Check if list items were created
@@ -119,12 +137,12 @@ class Menu {
 //Class for article object
 class Article {
     //The html article tag must be window attached, so it can be accessed
-    constructor() {
+    constructor(title, thisId) {
         window.articleTag = document.createElement("article");
         this.h1 = document.createElement("h1");
-        this.h1.appendChild(document.createTextNode("Summary"));
+        this.h1.appendChild(document.createTextNode(title));
         window.articleTag.appendChild(this.h1);
-        this.h1.setAttribute("id", "articleTitle")
+        this.h1.setAttribute("id", thisId)
         $("main")[0].appendChild(window.articleTag);
 
         //The article has sections, which will be created
@@ -146,8 +164,6 @@ class PageControl {
             obj.section.appendChild(obj.unorderedList.ul);
         else if(obj instanceof ImageText) {
             obj.section.appendChild(obj.image.img);
-            //The object pageEconomics contains an image, we need to add a specific ID for the CSS
-            obj.image.img.id = "economicsImage";
         }
     }
 }
@@ -189,22 +205,22 @@ And it also protects against any third party sending data pretending to be someo
 In this way though a third party can still interfere with the communications, because these 
 are unintelligable for them it is has the same result as if they were unable to interfere.`);
 
-window.pageImportance = new ListText("Importance", "Importance of security", `Imporance has become increasingly important as more and 
+window.pageImportance = new ListText("Importance", "Importance of security ▼", `Imporance has become increasingly important as more and 
 more computer systems are being utilzed. With more and more devices going on the internet, know as the Internet of Things, 
 the attack surface for hackers has also increased. Even cars have become a target for hackers as the computers inside 
-them are getting increasingly complex, but also increasingly connected with the internet.`,window.importanceList);
+them are getting increasingly complex, but also increasingly connected with the internet. Click on the title to hide/unhide the list.`,window.importanceList);
 
-window.pageSecurity = new ListText("Security", "Security attacks", `Computer security, or more formally known as IT security, is defined as the protection 
+window.pageSecurity = new ListText("Security", "Security attacks ▼", `Computer security, or more formally known as IT security, is defined as the protection 
 of computer systems from the theft and damage to their hardware, software or information, 
 as well as from disruption or misdirection of the services they provide
 Thus, protection includes the physical and software side. Security is a growing field of interest and
 importance because we are relying more and more on these devices. These devices are connected to the 
-Internet, and that brings a lot of trouble with it.`, window.securityList); 
+Internet, and that brings a lot of trouble with it. Click on the title to hide/unhide the list.`, window.securityList); 
 
-window.pageEconomics = new ImageText("Economics", "Statistics about security", `The image (click on title, to display) points out the statistics of 
+window.pageEconomics = new ImageText("Economics", "Statistics about security ▼", `The image (click on title, to display) points out the statistics of 
 the spending on cyber security in the recent years (in the U.S.) as a percentage of the GDP as 
 well as absolute in USD (dollars). We see an increasing spending over the years.`, 
-"http://publications.atlanticcouncil.org/cyberrisks//images/figure-8.jpg", "Cybersecurity spendings");
+"http://publications.atlanticcouncil.org/cyberrisks//images/figure-8.jpg", "Cybersecurity spendings", "economicsImage");
 
 //Create the menu, which will create the article, which on its turn, will put all the necessary objects on the page
 window.menu = new Menu(window.pageHistory.name, window.pageEncryption.name, window.pageImportance.name, 
